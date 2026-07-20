75c6df7fde01a2d0a2cc7391cd80e163e2498ff7
kevin.rebakure <kevinrebakure@gmail.com>
2026-07-20 16:15:32 +0200

    deps, eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  {
+    ignores: [
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/build/**",
+      "**/drizzle/**",
+      "apps/web/vite.config.ts",
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+);
