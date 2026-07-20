c54db19a66ac0b7e08f10fc1fd46377c9a125447
kevin.rebakure <kevinrebakure@gmail.com>
2026-07-20 16:11:24 +0200

    define routes

---
+++ apps/api/src/routes/index.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { canvasRouter } from "../canvas/routes";
+import { calendarRouter } from "../calendar/routes";
+import { whatsappRouter } from "../whatsapp/routes";
+import { plannerRouter } from "../planner/routes";
+import { schedulerRouter } from "../scheduler/routes";
+import { meRouter } from "../auth/routes";
+
+export const apiRouter = Router();
+
+apiRouter.use(meRouter);
+apiRouter.use("/canvas", canvasRouter);
+apiRouter.use("/calendar", calendarRouter);
+apiRouter.use("/whatsapp", whatsappRouter);
+apiRouter.use("/planner", plannerRouter);
+apiRouter.use("/schedule", schedulerRouter);
